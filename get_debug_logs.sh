5bfa4222f2d511e8d41139b17b87db0bc24c0255
Aleksei Tcelishchev <casualuser@gmail.com>
2026-02-11 12:06:29 -0500

    chore: cleanup temporary artifacts and logs, preserving features

--- get_debug_logs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/log show --style syslog --last 10m --predicate 'process == "kernel" OR process == "sysextd" OR sender contains "VirtualUSBDriver" OR process == "taskgated" OR process == "amfid"' > ~/debug_log.txt
--- get_remote_logs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-log show --style syslog --last 5m --predicate 'sender contains "kernel" or sender contains "driverkit" or process == "virtual-fido"' > /tmp/vm_debug.log
-grep -i -E "VirtualUSBDriver|virtual-fido|AMFI|signature|entitlement" /tmp/vm_debug.log
